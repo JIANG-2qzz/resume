25c3b3b815b6e6db2f918ca0c152c19b175e8446
JIANG-2qzz <jljl2284354365@163.com>
2023-07-02 14:13:43 +0800

    ceshi4

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import markdown from '../assets/resume.md?raw'
 import MdEditor from 'md-editor-rt';
 import 'md-editor-rt/lib/style.css';
-//3
+//4
 export default () => {
   const [text, setText] = useState(markdown);
   return <MdEditor modelValue={text} onChange={setText} previewOnly={true}/>;
